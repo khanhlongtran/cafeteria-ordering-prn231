3faa6c0cece7e0bcb796b1451a04d438c03a6bc1
Vu Duc Hien <vuduchien865@gmail.com>
2025-03-15 00:12:28 +0700

    Sửa thêm DTO
    Sửa thêm DTOd

--- CafeteriaOrdering.API/Models/MenuItem.cs
+++ CafeteriaOrdering.API/Models/MenuItem.cs
@@ -21,6 +21,7 @@ namespace CafeteriaOrdering.API.Models
         public DateTime UpdatedAt { get; set; }
         public int? CountItemsSold { get; set; }
         public bool? IsStatus { get; set; }
+        public string? Image { get; set; }
 
         public virtual Menu Menu { get; set; } = null!;
         public virtual ICollection<OrderItem> OrderItems { get; set; }
